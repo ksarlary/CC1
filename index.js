fc753aea1716848cdfced5aa542e6cc261906617
Sonia2 <ksarlary@gmail.com>
2025-10-30 10:04:36 +0100

    roll 5 dice

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 export function rollDice(){
     return Math.floor(Math.random() * 6) + 1;
 }
+
+export function rollFiveDice(){
+    const dice_1 = rollDice();
+    const dice_2 = rollDice();
+    const dice_3 = rollDice();
+    const dice_4 = rollDice();
+    const dice_5 = rollDice();
+    return dice_1 + dice_2 + dice_3 + dice_4 + dice_5;
+}
--- index.test.js
+++ index.test.js
@@ -1,11 +1,18 @@
 import { describe, it, expect } from "vitest";
 import { rollDice } from "./index";
+import { rollFiveDice } from "./index";
 
 describe("Roll dice", () => {
     it("Should return between 1 and 6 included", () => {
         const score = rollDice();
         const min = 1;
         const max = 6;
+        expect(score >= min && score <= max).toBe(true);
+    });
+    it("Should return the sum of 5 dice rolls between 5 and 30 included", () => {
+        const score = rollFiveDice();
+        const min = 5;
+        const max = 30;
         expect(score >= min && score <= max).toBe(true);
     });
 });
