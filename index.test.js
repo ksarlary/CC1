d91784f88c5979e29d5774143cf9d609e70e6638
Sonia2 <ksarlary@gmail.com>
2025-10-30 11:06:30 +0100

    rename

--- index.test.js
+++ index.test.js
@@ -1,101 +1,101 @@
 import { describe, it, expect } from "vitest";
 import { rollDice } from "./index";
 import { rollFiveDice } from "./index";
-import { findFigures } from "./index";
+import { findFIGUREs } from "./index";
 import { calculateRollPoints } from "./index";
-import { calculateTotalSum } from "./index";
+import { calculateTotalSUM } from "./index";
 
 describe("Roll dice", () => {
     it("Should return between 1 and 6 included", () => {
-        const score = rollDice();
-        const min = 1;
-        const max = 6;
-        expect(score >= min && score <= max).toBe(true);
+        const SCORE = rollDice();
+        const MIN = 1;
+        const MAX = 6;
+        expect(SCORE >= MIN && SCORE <= MAX).toBe(true);
     });
     it("Should return the sum of 5 dice rolls between 5 and 30 included", () => {
-        const rolls = rollFiveDice();
-        const min = 5;
-        const max = 30;
-        const sum = rolls.reduce((a, b) => a + b, 0);
-        expect(sum >= min && sum <= max).toBe(true);
+        const ROLLS = rollFiveDice();
+        const MIN = 5;
+        const MAX = 30;
+        const SUM = ROLLS.reduce((a, b) => a + b, 0);
+        expect(SUM >= MIN && SUM <= MAX).toBe(true);
     });
 });
 
-describe("Find figures", () => {
+describe("Find figuress", () => {
     it("Should find a Brelan and return 'Brelan'", () => {
-        const rolls = [2, 2, 2, 4, 5];
-        const figure = findFigures(rolls);
-        expect(figure).toBe("Brelan");
+        const ROLLS = [2, 2, 2, 4, 5];
+        const FIGURE = findFIGUREs(ROLLS);
+        expect(FIGURE).toBe("Brelan");
     });
     it("Should find a Carré and return 'Carré'", () => {
-        const rolls = [2, 2, 2, 2, 5];
-        const figure = findFigures(rolls);
-        expect(figure).toBe("Carré");
+        const ROLLS = [2, 2, 2, 2, 5];
+        const FIGURE = findFIGUREs(ROLLS);
+        expect(FIGURE).toBe("Carré");
     });
     it("Should find a Full and return 'Full'", () => {
-        const rolls = [2, 2, 2, 5, 5];
-        const figure = findFigures(rolls);
-        expect(figure).toBe("Full");
+        const ROLLS = [2, 2, 2, 5, 5];
+        const FIGURE = findFIGUREs(ROLLS);
+        expect(FIGURE).toBe("Full");
     });
     it("Should find a YAMS and return 'YAMS'", () => {
-        const rolls = [2, 2, 2, 2, 2];
-        const figure = findFigures(rolls);
-        expect(figure).toBe("YAMS");
+        const ROLLS = [2, 2, 2, 2, 2];
+        const FIGURE = findFIGUREs(ROLLS);
+        expect(FIGURE).toBe("YAMS");
     });
     it("Should find a Suite and return 'Suite' from 1 to 5", () => {
-        const rolls = [1, 2, 3, 4, 5];
-        const figure = findFigures(rolls);
-        expect(figure).toBe("Suite");
+        const ROLLS = [1, 2, 3, 4, 5];
+        const FIGURE = findFIGUREs(ROLLS);
+        expect(FIGURE).toBe("Suite");
     });
     it("Should find a Suite and return 'Suite' from 2 to 6", () => {
-        const rolls = [2, 3, 4, 5, 6];
-        const figure = findFigures(rolls);
-        expect(figure).toBe("Suite");
+        const ROLLS = [2, 3, 4, 5, 6];
+        const FIGURE = findFIGUREs(ROLLS);
+        expect(FIGURE).toBe("Suite");
     });
     it("Should find a Chance and return 'Chance'", () => {
-        const rolls = [1, 1, 3, 5, 6];
-        const figure = findFigures(rolls);
-        expect(figure).toBe("Chance");
+        const ROLLS = [1, 1, 3, 5, 6];
+        const FIGURE = findFIGUREs(ROLLS);
+        expect(FIGURE).toBe("Chance");
     });
 });
 
 describe("Calculate sum", () => {
     it("Should calculate points for Brelan", () => {
-        const rolls = [3, 3, 3, 4, 5];
-        const points = calculateRollPoints(rolls);
-        expect(points).toBe(28);
+        const ROLLS = [3, 3, 3, 4, 5];
+        const POINTS = calculateRollPoints(ROLLS);
+        expect(POINTS).toBe(28);
     });
     it("Should calculate points for Carré", () => {
-        const rolls = [3, 3, 3, 3, 5];
-        const points = calculateRollPoints(rolls);
-        expect(points).toBe(35);
+        const ROLLS = [3, 3, 3, 3, 5];
+        const POINTS = calculateRollPoints(ROLLS);
+        expect(POINTS).toBe(35);
     });
     it("Should calculate points for Full", () => {
-        const rolls = [3, 3, 3, 5, 5];
-        const points = calculateRollPoints(rolls);
-        expect(points).toBe(30);
+        const ROLLS = [3, 3, 3, 5, 5];
+        const POINTS = calculateRollPoints(ROLLS);
+        expect(POINTS).toBe(30);
     });
     it("Should calculate points for YAMS", () => {
-        const rolls = [3, 3, 3, 3, 3];
-        const points = calculateRollPoints(rolls);
-        expect(points).toBe(50);
+        const ROLLS = [3, 3, 3, 3, 3];
+        const POINTS = calculateRollPoints(ROLLS);
+        expect(POINTS).toBe(50);
     });
     it("Should calculate points for Suite", () => {
-        const rolls = [1, 2, 3, 4, 5];
-        const points = calculateRollPoints(rolls);
-        expect(points).toBe(40);
+        const ROLLS = [1, 2, 3, 4, 5];
+        const POINTS = calculateRollPoints(ROLLS);
+        expect(POINTS).toBe(40);
     });
     it("Should calculate points for Chance (sum of numbers)", () => {
-        const rolls = [1, 1, 3, 5, 6];
-        const points = calculateRollPoints(rolls);
-        expect(points).toBe(16);
+        const ROLLS = [1, 1, 3, 5, 6];
+        const POINTS = calculateRollPoints(ROLLS);
+        expect(POINTS).toBe(16);
     });
     it("Should calculate sum of points for multiple 5 dice rolls", () => {
-        const allRolls = [
+        const ALL_ROLLS = [
             [1, 1, 3, 5, 6], // Chance: 16
             [2, 2, 2, 4, 5], // Brelan: 28
             [3, 3, 3, 3, 3] // YAMS: 50
         ];
-        expect(calculateTotalSum(allRolls)).toBe(94);
+        expect(calculateTotalSUM(ALL_ROLLS)).toBe(94);
     });
 });
